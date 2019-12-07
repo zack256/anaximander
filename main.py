632428695cb1ffe8195ffa878c24d36bd012e948
anaximander <20821071+zack256@users.noreply.github.com>
2019-12-07 18:36:18 +0000

    added the basis for the news part of site

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_user import current_user, login_required, UserManager, UserMixin, SQLAlchemyAdapter, roles_required
 import config.config as config_app
+import config.paths
+import os
+import datetime
+import re
 
 app = Flask(__name__)
 config_app.config_app(app)
@@ -25,6 +29,7 @@ class User(db.Model, UserMixin):
 
     # Relationships
     roles = db.relationship('Role', secondary = 'user_roles', backref = db.backref('users', lazy='dynamic'))
+    news = db.relationship("NewsArticle", backref = "author")
 
 class Role(db.Model):
     id = db.Column(db.Integer(), primary_key = True)
@@ -41,6 +46,13 @@ class UserInvitation(db.Model):
     email = db.Column(db.String(255), nullable = False)
     invited_by_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     token = db.Column(db.String(100), nullable = False, server_default = '')
+
+class NewsArticle(db.Model):
+    __tablename__ = "news"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255), nullable = False, unique = True)
+    created = db.Column(db.DateTime())
+    author_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete = 'CASCADE'))
 
 user_manager = UserManager(app, db, User)
 
@@ -68,7 +80,27 @@ def make_initial_user(username, password, email):
     db.session.add(new_user)
     db.session.commit()
 
+def get_est_date():
+    return datetime.datetime.now() - datetime.timedelta(hours = 4)
 
+def check_if_valid_article_name(name):
+    return len(name) <= 255 and re.search(r"^[a-zA-Z0-9-]+$", name)
+
+def add_article_to_db(article_name, author):
+    # adds article in file sys to db.
+    if not check_if_valid_article_name(article_name):
+        app.logger.error("Invalid article name.")
+        return False
+    if not os.path.isfile(config.paths.NEWS_ARTICLES_DIR + article_name + ".txt"):
+        app.logger.error("File path does not exist.")
+        return False
+    new_article = NewsArticle()
+    new_article.name = article_name
+    new_article.created = datetime.datetime.now()   # stored in db as utc.
+    new_article.author_id = author.id
+    db.session.add(new_article)
+    db.session.commit()
+    return True
 
 
 
