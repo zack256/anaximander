3d09af62efaf5d452624e5b8d110213e9f996adf
anaximander <20821071+zack256@users.noreply.github.com>
2019-12-14 22:06:58 +0000

    start of diff.

---
+++ work/diff.py
@@ -0,0 +1,40 @@
+import lcs
+
+lcs_func = lcs.get_lcs
+
+def get_subtractions(s, lcs):
+    subtractions = []
+    left = 0
+    for c, i in enumerate(lcs):
+        right = s.find(i, left)
+        if left != right:
+            subtraction = [left, s[left: right]]
+            subtractions.append(subtraction)
+        left = right + 1
+    end = s[left : ]
+    if end != "":
+        ending_subtraction = [left, end]
+        subtractions.append(ending_subtraction)
+    return subtractions
+
+def get_additions(s, lcs):
+    additions = []
+    left = 0
+    for c, i in enumerate(lcs):
+        right = s.find(i, left)
+        if left != right:
+            addition = [c, s[left : right]]
+            additions.append(addition)
+        left = right + 1
+    end = s[left : ]
+    if end != "":
+        ending_addition = [len(lcs), end]
+        additions.append(ending_addition)
+    return additions
+
+def get_diff(a, b):
+    # Calculates the diff from a to b.
+    lcs = lcs_func(a, b)
+    subtractions = get_subtractions(a, lcs)
+    additions = get_additions(b, lcs)
+    return subtractions, additions
