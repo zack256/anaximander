f5fe99826ee35725d2771dd31334012a6469668b
anaximander <20821071+zack256@users.noreply.github.com>
2019-12-14 21:08:05 +0000

    removed the unused recursion method.

--- work/lcs.py
+++ work/lcs.py
@@ -12,26 +12,8 @@ def lcs_table_using_iteration(a, b):
                 l[i][j] = max(l[i - 1][j], l[i][j - 1])
     return l
 
-def lcs_table_using_recursion (a, b):
-    m = len(a); n = len(b)
-    if m == 0 or n == 0:
-        return 0
-    l = [[0 for i in range(m + 1)]] + [[0] + [-1 for j in range(m)] for k in range(n)]
-    result = lcs_recursion_helper(a, b, l)
-    return l
-
-def lcs_recursion_helper (a, b, l):
-    m = len(a); n = len(b)
-    if l[n][m] == -1:
-        if a[m - 1] == b[n - 1]:
-            l[n][m] = 1 + lcs_recursion_helper(a[:m - 1], b[:n - 1], l)
-        else:
-            l[n][m] = max(lcs_recursion_helper(a, b[:n - 1], l), lcs_recursion_helper(a[:m - 1], b, l))
-    return l[n][m]
-
-def get_lcs(a, b, use_iter = True):
-    func = lcs_table_using_iteration if use_iter else lcs_table_using_recursion
-    l = func(a, b)
+def get_lcs(a, b):
+    l = lcs_table_using_iteration(a, b)
     m = len(a); n = len(b)
     i = m; j = n
     len_lcs = l[n][m]
